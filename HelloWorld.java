5674ae7e9e451bccf636f00e60f04432a50c74cc
Mathew Ho <mathewho@xylog.com.my>
2018-04-09 10:52:33 +0800

    Test again!!!

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window. Testing purposes. Will push to GitHub
-        System.out.println("Hello, World");
+        System.out.println("Hello, World World World");
     }
 
 }
